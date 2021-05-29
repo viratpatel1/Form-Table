71f2a3251b6e8f45909607ad336b2c4a96358f43
Virat Patel <51950161+viratpatel1@users.noreply.github.com>
2021-05-29 12:26:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -68,46 +68,7 @@ function top2(value){
 }
 
 
-
 //To Clear the field every time when button is pressed
 btnClear.addEventListener('click',() => {
     inputs.forEach(input => input.value = '');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// fetch("https://www.guvi.in/model/v2/verify-mobile.php", {
-//   "headers": {
-//     "accept": "*/*",
-//     "accept-language": "en-US,en;q=0.9,mr;q=0.8",
-//     "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-//     "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"",
-//     "sec-ch-ua-mobile": "?0",
-//     "sec-fetch-dest": "empty",
-//     "sec-fetch-mode": "cors",
-//     "sec-fetch-site": "same-origin",
-//     "x-requested-with": "XMLHttpRequest"
-//   },
-//   "referrer": "https://www.guvi.in/dashboard",
-//   "referrerPolicy": "strict-origin-when-cross-origin",
-//   "body": "myData=%7B%22requestType%22%3A%22verifyAccount%22%2C%22authtoken%22%3A%22018b297211a3e036d61b0f6fcf21fd70bde03319f766920c0f8647ad36b34eb148a4b588363f32e1d8af83818638de7d82b9607338156276ad393bc0acda8294%22%7D",
-//   "method": "POST",
-//   "mode": "cors",
-//   "credentials": "include"
-// }).then(data => data.json())
-// .josn(x => console.log(x))
